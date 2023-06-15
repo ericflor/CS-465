b132e03bec5e825cf2d7e8adbb3cdae2707b0163
EricFlorence <Eric.Florence@aa.com>
2023-06-15 12:13:19 -0500

    troubleshooting jwt not a function error

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const jwt = require('express-jwt');
 
-const auth = jwt({
+const auth = jwt ({
   secret: process.env.JWT_SECRET,
   userProperty: "payload",
 });
